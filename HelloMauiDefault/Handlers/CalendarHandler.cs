ccc86a87a574182f33e86a76880d7e5b48329efa
Alp Eren Velioğlu <67262963+alpvelioglu@users.noreply.github.com>
2024-12-03 23:47:46 +0300

    IOS tamamlandı gibi çalışıyor mu şüpheli..

---
+++ HelloMauiDefault/Handlers/CalendarHandler.cs
@@ -0,0 +1,30 @@
+﻿using HelloMauiDefault.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMauiDefault.Handlers
+{
+    public partial class CalendarHandler
+    {
+        public static IPropertyMapper<ICalendarView, CalendarHandler> PropertyMapper = new PropertyMapper<ICalendarView, CalendarHandler>(ViewMapper)
+        {
+            [nameof(ICalendarView.FirstDayOfWeek)] = MapFirstDayOfWeek,
+            [nameof(ICalendarView.MinDate)] = MapMinDate,
+            [nameof(ICalendarView.MaxDate)] = MapMaxDate,
+            [nameof(ICalendarView.SelectedDate)] = MapSelectedDate,
+        };
+
+        public static CommandMapper<ICalendarView, CalendarHandler> CommandMapper = new(ViewCommandMapper);
+
+        public CalendarHandler(IPropertyMapper mapper, CommandMapper? commandMapper = null) : base(mapper, commandMapper)
+        {
+        }
+
+        public CalendarHandler() : this(PropertyMapper, CommandMapper)
+        {
+        }
+    }
+}
---
+++ HelloMauiDefault/Views/ICalendarView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMauiDefault.Views
+{
+    public interface ICalendarView : IView
+    {
+        DayOfWeek FirstDayOfWeek { get; }
+        DateTimeOffset MinDate { get; }
+        DateTimeOffset MaxDate { get; }
+        DateTimeOffset? SelectedDate { get; set; }
+        void OnSelectedDateChanged(DateTimeOffset? selectedDate);
+    }
+}
